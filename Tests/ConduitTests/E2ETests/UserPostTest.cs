1887940356790e15d7a7e679f745a7e0926fb316
mayankbhatt <mayank1004@gmail.com>
2022-05-17 23:14:19 -0700

    Update fixtures

--- Tests/ConduitTests/E2ETests/UserPostTest.cs
+++ Tests/ConduitTests/E2ETests/UserPostTest.cs
@@ -1,23 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using E2ETestCSharp.POM;
 using E2ETestCSharp.Tests.Fixtures;
-using Microsoft.Playwright;
-using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
-namespace E2ETestCSharp.Tests.E2ETests
+namespace E2ETestCSharp.Tests.ConduitTests.E2ETests
 {
-    public class LoginTests : PageTest
-    {
+	public class UserPostTest : FixtureWithUserSignedIn
+	{
         [Test]
         public async Task ShouldSignIn()
         {
-            var signinPage = new SignInPage(Page);
+            var signinPage = new SignInPage(page);
             await signinPage.GoTo();
             await signinPage.SignIn("interview@start.com", "password");
-            HomePage homePage = new HomePage(Page);
+            HomePage homePage = new HomePage(page);
             await Expect(homePage._userProfileLink).ToBeVisibleAsync();
 
         }
     }
 }
+
---
+++ Tests/Fixtures/TestFixtures.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace E2ETestCSharp.Tests.Fixtures
+{
+	[Parallelizable(ParallelScope.Self)]
+	public class TestFixtures
+	{
+		public TestFixtures()
+		{
+		}
+
+		IPlaywright playwright;
+		IBrowser browser;
+
+		[OneTimeSetUp]
+		public async Task LaunchBrowserAsync()
+        {
+			playwright = await Playwright.CreateAsync();
+			var firefox = playwright.Firefox;
+			browser = await firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+		}
+
+        protected IPage Page;
+		IBrowserContext context;
+
+		[SetUp]
+		public async Task CreateContextAndPage()
+        {
+			context = await browser.NewContextAsync();
+			Page = await context.NewPageAsync();
+
+        }
+
+		[TearDown]
+		public async Task CloseContext()
+		{
+            await context.CloseAsync();
+			await Page.CloseAsync();
+		}
+
+
+		[OneTimeTearDown]
+		public async Task CloseBrowser()
+        {
+			await browser.CloseAsync();
+		}
+	}
+}
+
