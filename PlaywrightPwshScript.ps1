50f71f9dc6584a17b7f04f1ff663c5c9eab1a4ff
mayank1004 <mayank1004@gmail.com>
2022-05-18 15:53:15 -0700

    Update CI changes

--- PlaywrightPwshScript.ps1
+++ PlaywrightPwshScript.ps1
@@ -1,3 +1,3 @@
-﻿$PlaywrightFileName = Join-Path $PSScriptRoot "bin/Debug"
+﻿$PlaywrightFileName = Join-Path $PSScriptRoot "bin/Debug/netcoreapp3.1"
 Get-ChildItem -Path $PlaywrightFileName -Name
 pwsh $PlaywrightFileName install
